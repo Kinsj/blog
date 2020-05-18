bb70a569509167f4a51b043f0668017f72bf125e
kin <1037671280@qq.com>
2020-05-18 20:47:30 +0800

    update

--- .temp/app-enhancers/global-components-1.js
+++
@@ -1,6 +0,0 @@
-import Vue from 'vue'
-
-Vue.component("Badge", () => import("D:\\workspace\\vuepress\\vuepress-demo\\node_modules\\@vuepress\\theme-default\\global-components\\Badge"))
-
-
-export default {}
